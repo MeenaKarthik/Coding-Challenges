6ae134ff4dd75f0adf01206619db9161c12556e4
Meena Karthik <meenafresh@gmail.com>
2023-05-28 17:07:55 -0500

    Week6_Challenge done

--- Week6_Challenge.js
+++ Week6_Challenge.js
@@ -1,26 +1,26 @@
 const input = require('readline-sync');
-let inputWord = input.question("Enter a word in lower case such that it is not in sequence and hides inside upper case letters: ");
+let str = input.question("Enter a word in lower case such that it is not in sequence and hides inside upper case letters: ");
 
-function detectWord(word){
+function detectWord(str){
     
-    word = word.match(/[a-zA-Z]/g);
-    console.log("array of word: "+word);
-    let hiddenWord = [];
+    str = str.match(/[a-zA-Z]/g);
+    console.log("array of str: "+str);
+    let hiddenstr = [];
     
-    if(word){
-        for(let i=0;i<word.length;i++){
+    if(str){
+        for(let i=0;i<str.length;i++){
 
-            if(word[i] == word[i].toLowerCase()){
-                hiddenWord.push(word[i]);
+            if(str[i] == str[i].toLowerCase()){
+                hiddenstr.push(str[i]);
             }
         }
 
-        if(hiddenWord.length>0)
-        return "Hidden word: "+hiddenWord.join('');  
+        if(hiddenstr.length>0)
+        return hiddenstr.join('');  
         else
         return "no lowercase letters in the given input string";  
     } else {
         return "The input string doesn't contain any letter";
     }
 }
-console.log(detectWord(inputWord));
+console.log(detectWord(str));
